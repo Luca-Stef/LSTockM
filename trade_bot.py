190aa84084460841451153b290c083f167377e13
Luca Stefanescu <lucastu2013@gmail.com>
2023-09-17 16:15:21 +0100

    m

--- trade_bot.py
+++ trade_bot.py
@@ -1,11 +1,7 @@
-from utils import *
+from utils import adjust_positions, 
 
-while True:
+if not trading_client.get_clock().is_open: print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Market closed, sleeping.')
 
-    if not trading_client.get_clock().is_open: print("Market closed, sleeping."); sleep_until(hour=15)
+adjust_positions()
 
-    adjust_positions()
-
-    print("Done for the day, sleeping until tomorrow"); sleep_until(hour=15)
-    new_comp = get_comp(position.symbol)
-    if set(new_comp) != set(current_comp): print(f"{position.symbol} composition has changed")
+print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Done for the day, sleeping until tomorrow')
